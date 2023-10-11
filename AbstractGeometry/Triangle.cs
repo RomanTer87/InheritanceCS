4f9f1e0d28b055d9de65ea98519350c38a293dc6
1 <1@DESKTOP-NU9I6RS>
2023-10-11 22:47:29 +0800

    AbstractGeometry in process

--- AbstractGeometry/Triangle.cs
+++ AbstractGeometry/Triangle.cs
@@ -5,10 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Security.Policy;
 
 namespace AbstractGeometry
 {
-	/*internal class Triangle:Shape
+	internal class Triangle:Shape
 	{
 		double side_one;
 		double side_two;
@@ -47,7 +48,25 @@ namespace AbstractGeometry
 		public Triangle(double side_one, double side_two, double side_three, int start_x, int start_y, int line_width,Color color)
 			:base(start_x, start_y, line_width, color)
 		{
-			double max_side = Triangle.;
+			double max_side = Math.Max(Math.Max(side_one, side_two), side_three);
+			double min_side = Math.Min(Math.Min(side_one, side_two), side_three);
+			double three_side = (side_one + side_two + side_three) - (max_side + min_side);
+			if (max_side >= (min_side + three_side)) max_side = (min_side + three_side) - 10;
+			SideOne = max_side; SideTwo= min_side;SideThree= three_side;
 		}
-	}*/
+		public override double GetArea()
+		{
+			double pperimetr = this.GetPerimeter() / 2;
+			return Math.Round(Math.Sqrt(pperimetr * (pperimetr - side_one) * (pperimetr - side_two) * (pperimetr - side_three)) * 10) / 10;
+		}
+		public override double GetPerimeter()
+		{
+			return side_one+side_two+side_three;
+		}
+		public override void Draw(PaintEventArgs e)
+		{
+			Pen pen = new Pen(Color, LineWidth);
+			e.Graphics.DrawPolygon;
+		}
+	}
 }
